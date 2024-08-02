3eaf028c64bfa7f416214f0910877d59e63bf1f5
Hammad Nawaz <sohaibnaw5@gmail.com>
2024-08-02 16:56:43 +0500

    first commit

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class OrdersService {
+
+
+  constructor(private readonly databaseService: DatabaseService) {}
+
+
+  async create(createOrderDto: Prisma.OrderCreateInput) {
+    const product = await this.databaseService.product.findUnique({
+      where: {id: createOrderDto.productId}
+    });
+    const totalPrice = product.price * createOrderDto.quantity;
+    return this.databaseService.order.create({
+      data:{
+        ...createOrderDto,
+        totalPrice
+      }
+    })
+  }
+
+  async findAll() {
+    return this.databaseService.order.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.order.findUnique({where:{id}});
+  }
+
+  async update(id: number, updateOrderDto: UpdateOrderDto) {
+    const order = await this.databaseService.order.findUnique({where:{id}});
+    const product = await this.databaseService.product.findUnique({where:{id}});
+    const totalPrice = product.price * order.quantity
+    return this.databaseService.order.update({
+      where:{id},
+      data:{
+        ...updateOrderDto,
+        totalPrice
+      }
+    })
+  }
+
+  async remove(id: number) {
+    return this.databaseService.order.delete({where: {id}});
+  }
+}
